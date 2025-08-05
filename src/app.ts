b51eed0c97a3420d4159ddf4a192d5e0aec2c564
Henry <kieuminhduc01@gmail.com>
2025-08-05 16:17:21 +0700

    #34. To chuc thu muc cho du an

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import 'dotenv/config';
+import webRoutes from './routes/web';
 
 const app = express();
 
 app.set('view engine','ejs');
 app.set('views',`${__dirname}/views/`);
 
- 
+webRoutes(app);
+
 app.listen(process.env.PORT, () => {
   console.log(`Server running ai http://localhost:${process.env.PORT}`);
   console.log(`env port: ${process.env.PORT}`)
