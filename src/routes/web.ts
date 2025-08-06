2e7945019cb041ef88aa820d5623a6450866378d
Henry <kieuminhduc01@gmail.com>
2025-08-06 10:54:46 +0700

    #38. Gioi hieu ve HTML form

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -1,12 +1,14 @@
-import express, { Express } from "express";
-import { getCreateUserPage, getHomePage } from "../controllers/user.controller";
+import express, { Express, Router } from "express";
+import { getCreateUserPage, getHomePage, postCreateUser } from "../controllers/user.controller";
 
-const router = express.Router();
+const router:Router = express.Router();
 
 const webRoutes = (app:Express) => {
     router.get("/", getHomePage);
 
     router.get("/create-user", getCreateUserPage);
+    router.post("/handle-create-user",postCreateUser)
+
 
     app.use('/', router);
 }
